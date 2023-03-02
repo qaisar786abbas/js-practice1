88525325c516c2249e8b8569af6f9a897c10736a
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-03-02 06:53:32 -0800

    added files

---
+++ practice1/app.js
@@ -0,0 +1,106 @@
+// var num = +prompt("Enter any number");
+
+// if(num > 0){
+//     alert("The number is positive.");
+// }
+// else if(num < 0){
+//     alert("The number is negative.");
+// }
+// else{
+//     alert("The number are equal.");
+// }
+
+
+
+
+
+// var arr = [4352647891]
+//  newArr = arr.sort("");
+// console.log(newArr);
+
+
+// function maximum(){
+// var arr = [23,11,13,24,43,59,100]
+// var max = arr[0];
+// for(var i=0; i<=arr.length; i++){
+//     if(arr[i]>max){
+//         max = arr[i];
+//     }
+// }
+// console.log("The maximum number is " + max);
+// document.write("The maximum number is " + max)
+// }
+// maximum();
+
+
+
+// var num = [12,43,55,22,5,8,1]
+// function minimum(){
+// var num = [12,43,55,22,5,8,1]
+// var min = num[0];
+// for(var i=0; i <= num.length; i++){
+//     if(num[i]<min){
+//         min = num[i];
+//     }
+// }
+// console.log("The minimum number is " + min);
+// document.write("The minimum number is " + min);
+// }
+// minimum();
+
+
+
+// var i;
+// for(var i=0; i<=30; i++){
+//     console.log(i);
+//     document.write(i , "<br/>");
+// }
+
+
+// var i;
+// for(var i=0; i<=30; i+=2){
+//     console.log(i);
+//     document.write(i , "<br/>");
+// }
+
+
+
+// var i;
+// for(var i=0; i<=20; i+=5){
+//     console.log(i);
+//     document.write(i , "<br/>");
+// }
+
+
+
+// var arr = prompt("Enter any number?");
+// var length = prompt("Enter the length");
+// for(var i=1; i<=length; i++){
+//     document.write(arr + "x" + i + "=" + arr*i ,  "<br/>");
+// }
+
+
+// var p = prompt("Enter any word?");
+// str =p.split("").reverse().join("");
+// // document.write(str);
+// if(p == str){
+//     console.log("The number is polindrom");
+// }
+// else{
+//     console.log("The word is not polindrom");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
